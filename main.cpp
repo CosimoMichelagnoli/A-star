cd26af18ac14e47866c6773dabdde3ab367ec45c
cosimo <cosimo.michelagnoli@stud.unifi.it>
2019-04-01 23:53:01 +0200

    cambio radicale mappa con int

--- main.cpp
+++ main.cpp
@@ -1,10 +1,130 @@
 #include <iostream>
 #include "Mappa.h"
 #include "Personaggio.h"
+#include "MapSearchNode.h"
 #include <iostream>
 using namespace std;
 int main() {
-    Mappa::Instance(1);
+    srand((unsigned int)time(NULL));
+    Mappa::Instance();
     Personaggio::Instance();
+    AStarSearch<MapSearchNode> astarsearch;
 
+    unsigned int SearchCount = 0;
+
+    const unsigned int NumSearches = 1;
+
+    while(SearchCount < NumSearches)
+    {
+
+        // Create a start state
+        MapSearchNode nodeStart;
+        nodeStart.x = rand()%Mappa::Instance()->getWidth();
+        nodeStart.y = rand()%Mappa::Instance()->getHeight();
+
+        // Define the goal state
+        MapSearchNode nodeEnd;
+        nodeEnd.x = rand()%Mappa::Instance()->getWidth();
+        nodeEnd.y = rand()%Mappa::Instance()->getHeight();
+
+        // Set Start and goal states
+
+        astarsearch.SetStartAndGoalStates( nodeStart, nodeEnd );
+
+        unsigned int SearchState;
+        unsigned int SearchSteps = 0;
+
+        do
+        {
+            SearchState = astarsearch.SearchStep();
+
+            SearchSteps++;
+
+#if DEBUG_LISTS
+
+            cout << "Steps:" << SearchSteps << "\n";
+
+			int len = 0;
+
+			cout << "Open:\n";
+			MapSearchNode *p = astarsearch.GetOpenListStart();
+			while( p )
+			{
+				len++;
+	#if !DEBUG_LIST_LENGTHS_ONLY
+				((MapSearchNode *)p)->PrintNodeInfo();
+	#endif
+				p = astarsearch.GetOpenListNext();
+
+			}
+
+			cout << "Open list has " << len << " nodes\n";
+
+			len = 0;
+
+			cout << "Closed:\n";
+			p = astarsearch.GetClosedListStart();
+			while( p )
+			{
+				len++;
+	#if !DEBUG_LIST_LENGTHS_ONLY
+				p->PrintNodeInfo();
+	#endif
+				p = astarsearch.GetClosedListNext();
+			}
+
+			cout << "Closed list has " << len << " nodes\n";
+#endif
+
+        }
+        while( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_SEARCHING );
+
+        if( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_SUCCEEDED )
+        {
+            cout << "Search found goal state\n";
+
+            MapSearchNode *node = astarsearch.GetSolutionStart();
+
+#if DISPLAY_SOLUTION
+            cout << "Displaying solution\n";
+#endif
+            int steps = 0;
+
+            node->PrintNodeInfo();
+            for( ;; )
+            {
+                node = astarsearch.GetSolutionNext();
+
+                if( !node )
+                {
+                    break;
+                }
+
+                node->PrintNodeInfo();
+                steps ++;
+
+            };
+
+            cout << "Solution steps " << steps << endl;
+
+            // Once you're done with the solution you can free the nodes up
+            astarsearch.FreeSolutionNodes();
+
+
+        }
+        else if( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_FAILED )
+        {
+            cout << "Search terminated. Did not find goal state\n";
+
+        }
+
+        // Display the number of loops the search went through
+        cout << "SearchSteps : " << SearchSteps << "\n";
+
+        SearchCount ++;
+
+        astarsearch.EnsureMemoryFreed();
+    }
+
+    return 0;
 }
